0abdcc73eb2d8f1cf0fc82dbf8e16ae9397c21a6
johnwlockwood <johnwlockwood@gmail.com>
2014-05-21 15:48:37 -0700

    Add descriptions to tap package files

---
+++ src/code.tackletronics.com/p/tap/bucket.go
@@ -0,0 +1,3 @@
+// A class that given a function, when called will
+// add the results to a deque. The user can access
+// the results of the calls to the function.
---
+++ src/code.tackletronics.com/p/tap/stream_tap.go
@@ -0,0 +1,6 @@
+// This function will take a collection of functions
+// and a stream of data and call each function with
+// each piece of data and then yield each piece of data.
+
+package stream_tap
+
